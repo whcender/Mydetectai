806f4a3f7fe8f194d5ee573ef671fb62d06bea78
ErtugrulArac <aracertugrul407@gmail.com>
2024-04-23 13:20:56 +0300

    video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Main from "@/components/main/index"
 import Table from "@/components/table/index"
+import Video from "@/components/video/index"
 
 export default function Home() {
   return (
     <div>
       <Main/>
       <Table/>
+      <Video/>
     </div>
   );
 }
---
+++ src/components/video/index.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { useState, ChangeEvent, FormEvent } from 'react';
+
+const VideoUpload = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [responseMessage, setResponseMessage] = useState<string>('');
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('video', selectedFile);
+
+    try {
+      const response = await fetch('https://aitest-qo53752v5a-lz.a.run.app/', {
+        method: 'POST',
+        body: formData
+      });
+      const data = await response.json();
+      setResponseMessage(data.message); // Assuming API returns a message
+    } catch (error) {
+      console.error('Error:', error);
+      setResponseMessage('An error occurred while uploading the video.');
+    }
+  };
+
+  return (
+    <div className='text-white'>
+      <h2>Video Upload</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept="video/*" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+      </form>
+      {responseMessage && (
+        <div>
+          <h3>Response Message:</h3>
+          <p>{responseMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VideoUpload;
