77fec644811bdd807171cb982f8b7d033118d591
whcender <heilender31@gmail.com>
2024-04-24 12:31:20 +0300

    router try

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -4,10 +4,10 @@ import VerificationMail  from "@/components/email/verificationMail"
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-    const coniformLink = `https://tawzenstore.vercel.app/auth/new-verification?token=${token}`;
+    const coniformLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "info@arlanqrmenu.com",
         to: email,
         subject: "Selam! E Posta Adresini Onayla",
         react: VerificationMail({ confirmLink: coniformLink }) as React.ReactElement,
@@ -18,7 +18,7 @@ export const sendResetEmail = async (email: string, token: string) => {
     const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
 
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "info@arlanqrmenu.com",
         to: email,
         subject: "Selam! Şifreni Yenile",
         html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`,
