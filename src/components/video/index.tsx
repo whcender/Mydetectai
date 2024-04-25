122a57fc051c84dbbbc31648fea8d749449f9205
whcender <heilender31@gmail.com>
2024-04-25 16:46:30 +0300

    video wait

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Button } from '../ui/button';
 
 const VideoUpload = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [responseMessage, setResponseMessage] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false); // Yükleniyor durumunu tutmak için
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
@@ -15,9 +17,11 @@ const VideoUpload = () => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    if (!selectedFile) {
+    if (!selectedFile || loading) { // Eğer dosya seçilmemişse veya yükleme işlemi devam ediyorsa, işlemi durdur
       return;
     }
+
+    setLoading(true); // Yükleme işlemi başladı
 
     const formData = new FormData();
     formData.append('file', selectedFile);
@@ -39,19 +43,23 @@ const VideoUpload = () => {
     } catch (error) {
       console.error('Error:', error);
       setResponseMessage('An error occurred while uploading the video.');
+    } finally {
+      setLoading(false); // İşlem tamamlandı, yükleme durumunu false olarak ayarla
     }
   };
 
   return (
-    <div className='text-white'>
-      <h2>Video Upload</h2>
+    <div className='text-white w-[80%] m-auto'>
+      <h2 className='mt-6'>Video Upload</h2>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
+        <Button variant={'destructive'} type="submit" disabled={!selectedFile || loading}> {/* Buton yalnızca dosya seçilmişse ve yükleme işlemi yoksa aktif olacak */}
+          {loading ? 'Uploading...' : 'Upload'}
+        </Button>
       </form>
       {responseMessage && (
         <div>
-          <h3>Response Message:</h3>
+          <h3>Sonuç:</h3>
           <p>{responseMessage}</p>
         </div>
       )}
