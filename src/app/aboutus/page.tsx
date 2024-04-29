42538e9da059d41aa00ea9dfae30b41332a6fb5c
ErtugrulArac <aracertugrul407@gmail.com>
2024-04-29 21:32:38 +0300

    giriş

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
 
-
-
 const page = () => {
   return (
     <div className='text-white'>
@@ -10,7 +8,6 @@ const page = () => {
         <p className='font-roboto text-center text-base'>-become one of us-</p>
         <p className='font-roboto max-md:text-5xl font-bold text-7xl'>Who Are We</p>
         <p className='font-nato w-[60%] max-md:w-[90%] text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsam quam molestiae iure consequatur quaerat dolorum, eum quae deleniti voluptate vitae reprehenderit at doloremque atque obcaecati iste excepturi temporibus neque?</p>
-
       </div>
       <div className='about mt-20  text-black flex flex-col  items-center justify-center'>
         <div className=' w-[60%] max-md:w-[90%] m-auto max-md:flex-col items-center gap-28 justify-center flex h-[450px] max-md:h-[600px] bg-[#f4f4f4]'>
@@ -27,15 +24,15 @@ const page = () => {
             </div>
 
           </div>
-          <img className='size-96 max-md:hidden' src="hello.svg" alt="" />
+          <img className='size-96 max-lg:hidden max-2xl:hidden max-md:hidden' src="hello.svg" alt="" />
         </div>
+        {/* <br />
         <br />
         <br />
-        <br />
         <br className='max-md:hidden' />
         <br className='max-md:hidden' />
         <br className='max-md:hidden' />
-        <br className='max-md:hidden' />
+        <br className='max-md:hidden' /> */}
       </div>
 
     </div>
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,7 +4,7 @@ const index = () => {
     return (
         <div className='text-white   mt-36 max-lg:mt-36'>
             <div className='w-[80%]  max-lg:w-[90%] m-auto max-lg:flex-col flex justify-between items-center max-md:gap-6'>
-                <h1 className='text-[55px] max-lg:text-[40px] max-md:text-[30px] w-[45%] max-lg:w-[90%] font-darker'>Harnessing Artificial Intelligence Tools for <br /> Naturally Image Generator</h1>
+                <h1 className='text-[55px] max-lg:text-[40px] max-md:text-[30px] w-[35%] max-xl:w-[70%] max-lg:w-[90%] max-2xl:text-[35px]  font-darker'>Harnessing Artificial Intelligence Tools for <br /> Naturally Image Generator</h1>
                 <div className='flex w-[50%] max-lg:w-[90%] flex-col gap-2 max-md:gap-2  justify-end'>
                     <div className='flex gap-1 '>
                         <img className='size-10 max-lg:size-8' src="ates.svg" alt="" />
